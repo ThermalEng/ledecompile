f999815c1955eab80d64e7d643155511b397a2f3
ThermalEng <38897886+ThermalEng@users.noreply.github.com>
2021-12-16 12:46:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@
 
 # Add a feed source
 echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
-echo 'src-git tcpdump https://github.com/KFERMercer/luci-app-tcpdump' >>feeds.conf.default
+git clone https://github.com/KFERMercer/luci-app-tcpdump.git ./package/luci-app-tcpdump
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
